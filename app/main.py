fb171f782a6c41cd7859cfbd1164578ed007c2f7
muqtasid <muqtasidali1109@gmail.com>
2025-08-30 00:11:20 +0500

    updated code

--- app/main.py
+++ app/main.py
@@ -1,11 +1,10 @@
 from fastapi import FastAPI
-from app import models, database, routes  # changed from relative to absolute import
+from app import models, database, routes
 
-# Create tables
-models.Base.metadata.create_all(bind=database.engine)
-
-# FastAPI app
 app = FastAPI(title="Invoice Billing System")
 
-# Include routes
+@app.on_event("startup")
+def on_startup():
+    models.Base.metadata.create_all(bind=database.engine)
+
 app.include_router(routes.router)
